09adc2804b498648430fe4c961a748819cde3228
jakehobbs <jakehobbs@gmail.com>
2021-12-13 12:25:02 -0800

    lazy load funraise script

--- components/Support.tsx
+++ components/Support.tsx
@@ -43,7 +43,7 @@ export const Support = () => {
       </section>
       <Script
         id="funraise-script"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `
           (function(f,u,n,r,a,i,s,e){var data={window:window,document:document,tag:"script",data:"funraise",orgId:f,uri:u,common:n,client:r,script:a};var scripts;var funraiseScript;data.window[data.data]=data.window[data.data]||[];if(data.window[data.data].scriptIsLoading||data.window[data.data].scriptIsLoaded)return;data.window[data.data].loading=true;data.window[data.data].push("init",data);scripts=data.document.getElementsByTagName(data.tag)[0];funraiseScript=data.document.createElement(data.tag);funraiseScript.async=true;funraiseScript.src=data.uri+data.common+data.script+"?orgId="+data.orgId;scripts.parentNode.insertBefore(funraiseScript,scripts)})("472c107b-a760-4be2-b990-81c429da14d5","https://assets.funraise.io","/widget/common/2.0","/widget/client","/inject-form.js");
