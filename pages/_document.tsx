bc8ab75388735e29f2e7c9b342b40d8e3ef64bb1
Jake Hobbs <jakehobbs@gmail.com>
2021-12-13 14:23:20 -0800

    font fallback

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,11 @@ export default function Document() {
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css?family=Varela+Round&display=optional"
+          href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap"
           rel="stylesheet"
         />
         <link
-          href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&display=optional"
+          href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&display=swap"
           rel="stylesheet"
         />
       </Head>
