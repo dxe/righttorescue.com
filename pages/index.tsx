40f5b344a60f5c86dc84d7d39dbadb2014f4bbca
CassieTechWizard <78454763+CassieTechWizard@users.noreply.github.com>
2022-05-31 15:51:52 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -98,7 +98,12 @@ const Home: NextPage = () => {
         </CasesSection.CaseList>
       </CasesSection>
       <Announcements>
-         <Announcement title="Beaver County, Utah" date="May 30, 2022">
+         <Announcement
+          title="Beaver County, Utah"
+          date= "May 30, 2022"
+          image="sf-ut.jpg"
+          featured
+        > 
           Trial dates are set for Wayne and Paul in Utah: Sept. 9-16. We are organizing a convergence in Utah to show support for animal rescue, build community with one another, and take action together to expose Smithfield’s Deathstar to the world. You can register at&nbsp;
           <a
             href="https://docs.google.com/forms/d/e/1FAIpQLSekKhKSd8KpIP1C3aVwfZ1GmeUd4VfvwuWtm3QhbN-NXK-HlQ/viewform"
