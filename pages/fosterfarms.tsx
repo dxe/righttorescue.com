53fee52ac3d2274cce9ac8887eb04b41f92b2772
CassieTechWizard <78454763+CassieTechWizard@users.noreply.github.com>
2022-06-29 17:56:23 -0700

    Update fosterfarms.tsx

--- pages/fosterfarms.tsx
+++ pages/fosterfarms.tsx
@@ -18,7 +18,7 @@ const FosterFarms: NextPage = () => {
       </Header>
       <Details image="Foster Farms rescue shot.png">
         In September 2021, two activists rescued birds from a stopped slaughter truck that was heading into the Foster Farms slaughterhouse in Livingston, CA.
-        They received larceny charges in the mail in Spring 2022 and had their first court date on June 15, 2022 in Merced Superior Court, where they entered not guilty pleas via their attorney. They are due back in court in September. Stay tuned for more information.  
+        They received theft charges in the mail in Spring 2022 and had their first court date on June 15, 2022 in Merced Superior Court, where they entered not guilty pleas via their attorney. They are due back in court in September. Stay tuned for more information.  
       </Details>
       <Defendants>
         <Bio name="Alexandra Paul" image="ap.jpg">
