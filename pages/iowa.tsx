998d9527595fcccc0d282a7e9d019ce1bd61049d
CassieTechWizard <78454763+CassieTechWizard@users.noreply.github.com>
2022-07-13 15:34:55 -0700

    Update iowa.tsx

--- pages/iowa.tsx
+++ pages/iowa.tsx
@@ -95,6 +95,10 @@ const Iowa: NextPage = () => {
           href="https://www.washingtonpost.com/business/second-case-dropped-against-man-who-exposed-iowa-hog-deaths/2022/01/20/d371fd94-7a1f-11ec-9dce-7313579de434_story.html"
           iFramelyUrl="https://cdn.iframe.ly/tY7dxuc?card=small"
         />
+        <Article
+          href="https://www.theguardian.com/world/2022/jan/22/an-animal-rights-activist-was-in-court-on-criminal-charges-why-was-the-case-suddenly-dismissed"
+          iFramelyUrl="https://cdn.iframely.net/UQEk06s?card=small"
+        />
       </Press>
     </>
   );
