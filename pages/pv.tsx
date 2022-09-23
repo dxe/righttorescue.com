e87488e812791900eb58c478e781948d31993365
CassieTechWizard <78454763+CassieTechWizard@users.noreply.github.com>
2022-09-22 18:00:50 -0700

    Update pv.tsx

--- pages/pv.tsx
+++ pages/pv.tsx
@@ -31,6 +31,20 @@ const PleasantValley: NextPage = () => {
         Appellate Division rejected our appeal by affirming the judge's original
         decision. So the restitution ruling still stands, but no one is paying. 
       </Details>
+      <Press>
+        <Article
+          href="https://www.nytimes.com/2016/10/21/business/video-reveals-how-cage-free-hens-live-animal-advocates-say.html"
+          iFramelyUrl="https://cdn.iframe.ly/tDwUC0Z?card=small"
+        />
+        <Article
+          href="/https://www.washingtonpost.com/news/animalia/wp/2016/10/20/her-head-has-been-torn-off-activists-film-alleges-abuse-at-costco-linked-cage-free-egg-farm/"
+          iFramelyUrl="https://cdn.iframe.ly/b6ncpT6?card=small"
+        />
+        <Article
+          href="https://www.sfgate.com/news/article/Animal-activist-charged-after-taking-hen-from-10907963.php"
+          iFramelyUrl="https://cdn.iframe.ly/H6afFBH?card=small"
+        />
+      </Press>
     </>
   );
 };
