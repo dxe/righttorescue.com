bab7598a1247004d068ce30e6920b4504a8bc543
CassieTechWizard <78454763+CassieTechWizard@users.noreply.github.com>
2022-12-14 16:01:23 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ import { FeaturedCase } from "../components/HomePage/FeaturedCase";
 const Home: NextPage = () => {
   return (
     <>
-      <MastHead image="sf-ut.jpg" takeActionPage={undefined} />
+      <MastHead image="alexandra2.jpg" takeActionPage={undefined} />
       <FeaturedCase
         name="Sunrise Farms & Reichardt Duck Farm"
         page="sunrise"
