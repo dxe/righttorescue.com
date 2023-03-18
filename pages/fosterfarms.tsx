3ea399a82b63d552e0f6e85f906a5202fb8e5a9c
CassieTechWizard <78454763+CassieTechWizard@users.noreply.github.com>
2023-03-17 17:59:00 -0700

    Update fosterfarms.tsx

--- pages/fosterfarms.tsx
+++ pages/fosterfarms.tsx
@@ -48,7 +48,7 @@ const FosterFarms: NextPage = () => {
             target="_blank"
           >
             Twitter.
-          </a
+        </a>
           &nbsp; Alexandra Paul and Alicia Santurio were facing theft charges for rescuing two sick birds, Jax and Ethan, from a stopped slaughter
         truck that was heading into the Foster Farms slaughterhouse in
         Livingston, CA. The same day, DxE released&nbsp;
