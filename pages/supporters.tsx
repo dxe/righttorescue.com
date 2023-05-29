7d82e72b245277f20879d5b6169a00138512b79e
Matthew Zirbel <matthew.d.zirbel@gmail.com>
2023-05-29 12:01:01 -0700

    Add OGAN and ER to supporters page

--- pages/supporters.tsx
+++ pages/supporters.tsx
@@ -7,100 +7,110 @@ const Supporters: NextPage = () => {
   return (
     <>
       <SupportersSection>
-           <Supporter
-            name="sf"
-            image="logos/sf.jpg"
-          />
+        <Supporter
+          name="sf"
+          image="logos/sf.jpg"
+        />
         
         <Supporter
-            name="sepastopol"
-            image="logos/sebastopol.png"
-          />
-        
-           <Supporter
-            name="cob"
-            image="logos/cob.jpg"
-          />
+          name="sepastopol"
+          image="logos/sebastopol.png"
+        />
         
         <Supporter
-            name="nlg"
-            image="logos/nlg.jpg"
-          />
+          name="cob"
+          image="logos/cob.jpg"
+        />
         
-           <Supporter
-            name="save"
-            image="logos/save.jpg"
-          />
+        <Supporter
+          name="nlg"
+          image="logos/nlg.jpg"
+        />
         
-          <Supporter
-            name="utv"
-            image="logos/utv.jpg"
-          />
+        <Supporter
+          name="save"
+          image="logos/save.jpg"
+        />
         
-          <Supporter
-            name="ap"
-            image="logos/ap.jpg"
-          />
+        <Supporter
+          name="utv"
+          image="logos/utv.jpg"
+        />
         
-          <Supporter
-            name="cb"
-            image="logos/cb.jpg"
-          />
-        
-         <Supporter
-            name="gafs"
-            image="logos/gafs.jpg"
-          />
-        
-          <Supporter
-            name="apex"
-            image="logos/apex.jpg"
-          />
+        <Supporter
+          name="ap"
+          image="logos/ap.jpg"
+        />
 
-          <Supporter
-            name="boaa"
-            image="logos/boaa.jpg"
-          />
+        <Supporter
+          name="cb"
+          image="logos/cb.jpg"
+        />
 
-          <Supporter
-            name="dva"
-            image="logos/dva.jpg"
-          />
+        <Supporter
+          name="gafs"
+          image="logos/gafs.jpg"
+        />
 
-          <Supporter
-            name="ida"
-            image="logos/ida.jpg"
-          />
+        <Supporter
+          name="apex"
+          image="logos/apex.jpg"
+        />
 
-          <Supporter
-            name="peta"
-            image="logos/peta.jpg"
-          />
+        <Supporter
+          name="boaa"
+          image="logos/boaa.jpg"
+        />
 
-          <Supporter
-            name="uarc"
-            image="logos/uarc.jpg"
-          />
+        <Supporter
+          name="dva"
+          image="logos/dva.jpg"
+        />
 
-          <Supporter
-            name="alt"
-            image="logos/animal_liberation_temple.png"
-          />
+        <Supporter
+          name="ida"
+          image="logos/ida.jpg"
+        />
 
-          <Supporter
-            name="happyhen"
-            image="logos/happy_hen.png"
-          />
+        <Supporter
+          name="peta"
+          image="logos/peta.jpg"
+        />
 
-          <Supporter
-            name="animal alliance network"
-            image="logos/animal_alliance_network.png"
-          />
+        <Supporter
+          name="uarc"
+          image="logos/uarc.jpg"
+        />
 
-          <Supporter
-            name="animal rising"
-            image="logos/animal_rising.png"
-          />
+        <Supporter
+          name="alt"
+          image="logos/animal_liberation_temple.png"
+        />
+
+        <Supporter
+          name="happyhen"
+          image="logos/happy_hen.png"
+        />
+
+        <Supporter
+          name="animal alliance network"
+          image="logos/animal_alliance_network.png"
+        />
+
+        <Supporter
+          name="animal rising"
+          image="logos/animal_rising.png"
+        />
+
+        <Supporter
+          name="Oil & Gas Action Network"
+          image="logos/oil_gas_action_network.png"
+        />
+
+        <Supporter
+          name="animal rising"
+          image="logos/extinction_rebellion.png"
+        />
 
       </SupportersSection>
     </>
