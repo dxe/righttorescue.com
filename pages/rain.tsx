3261368586d353d77743155257968250f1c475c1
CassieTechWizard <78454763+CassieTechWizard@users.noreply.github.com>
2023-06-20 13:03:07 -0700

    Update rain.tsx

--- pages/rain.tsx
+++ pages/rain.tsx
@@ -35,8 +35,7 @@ const Rain: NextPage = () => {
       </Details>
       <Defendants>
         <Bio name="Wayne Hsiung" image="sf-ut-wayne.jpg">
-          Wayne is an attorney and co-founder of the animal rights network,
-          Direct Action Everywhere. He has been a community organizer for over
+          Wayne is an attorney and co-founder of The Simple Heart. He has been a community organizer for over
           20 years.
         </Bio>
       </Defendants>
