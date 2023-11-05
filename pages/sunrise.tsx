14718a83c312742dcb5991bf3441546dbf19789b
CassieTechWizard <78454763+CassieTechWizard@users.noreply.github.com>
2023-11-05 12:37:33 -0800

    Update sunrise.tsx

--- pages/sunrise.tsx
+++ pages/sunrise.tsx
@@ -35,10 +35,8 @@ const Sunrise: NextPage = () => {
         been arrested on felony charges for taking nonviolent direct action to
         help suffering animals at factory farms in the county. These charges
         resulted in the five-year prosecution of a handful of activists and
-        ultimately, the seven-week trial of one remaining defendant, Wayne
-        Hsiung, in September to October 2023. We need your help to elevate the
-        #SonomaRescueTrial and demand that the District Attorney finally
-        prosecute the real crimes that are happening to animals.
+        ultimately, the eight-week trial of one remaining defendant, Wayne
+        Hsiung. On November 2, 2023, after 6 days of jury deliberation, Wayne was convicted of felony conspiracy and two counts of misdemeanor trespass. The jury could not reach a unanimous verdict on a second felony conspiracy charge, resulting in a “hung jury” mistrial on that charge. Wayne was cuffed and immediately taken into custody. He has a sentencing hearing set for November 30th. He will be in jail until then, without bail. Join us at the sentencing hearing to support Wayne and defend the #RightToRescue. The event page is at: dxe.io/sentencing.
       </Details>
 
       <Defendants>
