04c27f5666b0133423df3a4528cedcebfdf82d3c
CassieTechWizard <78454763+CassieTechWizard@users.noreply.github.com>
2023-11-05 13:37:25 -0800

    Update sunrise.tsx

--- pages/sunrise.tsx
+++ pages/sunrise.tsx
@@ -33,8 +33,7 @@ const Sunrise: NextPage = () => {
             target="_blank"
           >
             Direct Action Everywhere
-          </a> have been
-        exposing violations of animal cruelty law at factory farms in Sonoma
+          </a> have been exposing violations of animal cruelty law at factory farms in Sonoma
         County, including Sunrise Farms and Reichardt Duck Farm. Instead of
         prosecuting these companies, the Sonoma County District Attorney has
         gone after the whistleblowers and animal rescuers. Over 100 people have
@@ -45,7 +44,7 @@ const Sunrise: NextPage = () => {
         Hsiung.
       </Details>
 
-      <Details image="wayne cuffed at court.jpeg">
+      <Details image="wayne cuffed.png">
         On November 2, 2023, after 6 days of jury deliberation, Wayne was convicted of three of the four charges in the #SonomaRescueTrial. This includes felony conspiracy and misdemeanor trespass at Sunrise Farms and misdemeanor trespass at Reichardt Duck Farm. The jury could not reach a unanimous verdict on a second felony conspiracy charge, resulting in a “hung jury” mistrial on that charge. Wayne was cuffed and immediately taken into custody. He has a sentencing hearing set for November 30th at the Superior Court of Sonoma (600 Administration Dr. Santa Rosa, CA). He will be in jail until then, without bail. {" "}
           <a
             href="https://www.facebook.com/events/1055213158842795"
