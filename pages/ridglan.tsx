0837c9c8594664ba0edd0b28aed07b378c7da699
CassieTechWizard <78454763+CassieTechWizard@users.noreply.github.com>
2024-01-17 17:19:09 -0800

    Update ridglan.tsx

--- pages/ridglan.tsx
+++ pages/ridglan.tsx
@@ -29,9 +29,9 @@ const RidglanFarms: NextPage = () => {
         entered the facility and documented the filthy conditions and the
         psychological trauma of the dogs spinning endlessly inside small cages.
         The team rescued three beagles. Although the investigation and rescue
-        happened years ago, the charging documents were filed in August 2021.
+        happened in 2017, the charging documents were filed in August 2021.
         Eva, Paul, and Wayne are facing felony burglary and felony theft
-        charges. Trial is currently scheduled for March 15-22, 2024. This Right to Rescue case is an important opportunity to show the world
+        charges. Trial is currently scheduled for March 18-22, 2024. This Right to Rescue case is an important opportunity to show the world
         the horrors inflicted on humankind’s supposed best friend and to move
         toward ending experimentation on dogs and other animals.
       </Details>
