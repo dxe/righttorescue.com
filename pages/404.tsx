c8bff06aa35cecf83e3b46145bbbc58bef7c3612
Jake Hobbs <jakehobbs@gmail.com>
2024-03-20 01:41:57 -0700

    redirect from 404 page to lowecase page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
 export default function Custom404() {
+  const [isLoaded, setLoaded] = useState(false);
+  const router = useRouter();
+  // If a user tries to go to a path with uppercase letters, redirect them to the lowercase version.
+  useEffect(() => {
+    const lowerAsPath = router.asPath.toLowerCase();
+    if (router.asPath !== lowerAsPath) {
+      router.push(lowerAsPath);
+      return;
+    }
+    setLoaded(true);
+  }, [router]);
+
   return (
     <header className="masthead" style={{ backgroundColor: "#161616" }}>
       <div
@@ -12,7 +27,9 @@ export default function Custom404() {
         "
       >
         <div className="mx-auto text-center">
-          <h2 className="text-white mx-auto my-0">404 Page Not Found</h2>
+          {isLoaded && (
+            <h2 className="text-white mx-auto my-0">404 Page Not Found</h2>
+          )}
         </div>
       </div>
     </header>
