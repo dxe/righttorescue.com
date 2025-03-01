b47a19fa1b15c617b58e4575b2f31fa0f373c864
DxEelaine <129798548+DxEelaine@users.noreply.github.com>
2025-02-28 23:20:29 -0800

    Update petalumapoultry.tsx

--- pages/petalumapoultry.tsx
+++ pages/petalumapoultry.tsx
@@ -201,6 +201,10 @@ const Reichardt: NextPage = () => {
           href="https://sonomastatestar.com/37329/news/activists-face-harsh-penalties-for-exposing-cruelty/"
           iFramelyUrl="https://cdn.iframe.ly/rm0SXvA?card=small"
         />
+         <Article
+          href="https://www.publicnewsservice.org/2025-01-30/animal-welfare/trial-ahead-for-ca-animal-activist-accused-of-trespassing-theft-conspiracy/a94966-1"
+          iFramelyUrl="https://cdn.iframe.ly/l7LMcpf?card=small"
+        />
       </Press>
     </>
   );
