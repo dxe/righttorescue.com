5840535b1db1ca3f5335a556cf899f86ccaa272c
CassieTechWizard <78454763+CassieTechWizard@users.noreply.github.com>
2025-04-30 18:19:37 -0700

    Update petalumapoultry.tsx

--- pages/petalumapoultry.tsx
+++ pages/petalumapoultry.tsx
@@ -93,8 +93,7 @@ const Reichardt: NextPage = () => {
         </p>
         <p>
           After a back-and-forth where certain charges were dismissed, filed, and dismissed again, Zoe is now facing one felony conspiracy charge and four misdemeanors, including two types of trespass, tampering
-          with a vehicle, and theft. She faces a potential maximum sentence of 5 and a half years in prison. Zoe's trial was supposed to begin this May, but it was delayed because the prosecutors requested more time to prepare. We will share the new trial dates as soon as we have them.
-          Zoe's next court date is April 30th, when her attorney will argue for subpoenas of Petaluma Poultry's records regarding animal welfare and disease rates as well as records from the CA Department of Food and Agriculture (CDFA). Stay tuned for an update after that.
+          with a vehicle, and theft. She faces a potential maximum sentence of 5 and a half years in prison. Zoe's trial was supposed to begin in May, but it was delayed to September because the prosecutors requested more time to prepare. The #PerdueTrial will now start September 15, 2025, and it's expected to last 2-3 weeks. There is a pre-trial hearing set for June 13 to hear argument on defense's motion to use the affirmative defense of necessity, which applies when someone acts in an emergency situation in order to prevent a harm from happening, in this case to protect animals in need of urgent medical care and to prevent potential public health impacts to consumers.
         </p>
       </Details>
 
