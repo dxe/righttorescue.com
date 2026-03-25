efb8258cf35c2fbd696d538ecf5091943d634637
Alexander Taylor <ajxtaylor@gmail.com>
2026-03-25 04:25:53 +0000

    fix: prevent GTM snippet escaping
    prevent GTM snippet escaping by injecting script with Next Script
    component.
    
    this was causing:
    Uncaught SyntaxError: Unexpected token '&' (at (index):4:29)
    
    the quote was getting escaped in:
    w[l].push({ "gtm.start": ...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,21 +40,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta charSet="utf-8" />
-        {/* Google Tag Manager part 1 of 2 */}
-        <script id="google-tag-manager">
-          {`
-            (function (w, d, s, l, i) {
-                w[l] = w[l] || [];
-                w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-                var f = d.getElementsByTagName(s)[0],
-                  j = d.createElement(s),
-                  dl = l != "dataLayer" ? "&l=" + l : "";
-                j.async = true;
-                j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-                f.parentNode.insertBefore(j, f);
-              })(window, document, "script", "dataLayer", "${GTM_CONFIG}");
-          `}
-        </script>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
@@ -67,6 +52,25 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" type="image/png" href="favicon.ico" />
         <title>Right to Rescue</title>
       </Head>
+      {/* Google Tag Manager part 1 of 2 */}
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function (w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+                var f = d.getElementsByTagName(s)[0],
+                  j = d.createElement(s),
+                  dl = l != "dataLayer" ? "&l=" + l : "";
+                j.async = true;
+                j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+                f.parentNode.insertBefore(j, f);
+              })(window, document, "script", "dataLayer", "${GTM_CONFIG}");
+          `,
+        }}
+      />
 
       {/* Google Tag Manager part 2 of 2*/}
       <noscript>
